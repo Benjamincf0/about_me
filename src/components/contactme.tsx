a4fe3d6ad64731137bfe3dd50c80ee9664b6339f
Benjamin Curis-Friedman <bencf99@gmail.com>
2026-07-25 02:07:31 -0400

    w form

--- src/components/contactme.tsx
+++ src/components/contactme.tsx
@@ -8,12 +8,21 @@ enum ContactFormStates {
 }
 
 export default function ContactForm() {
-  const [contactFormState, setContactFormState] = useState(ContactFormStates.UNSENT);
+  const [contactFormState, setContactFormState] = useState(
+    ContactFormStates.UNSENT,
+  );
+
+  const buttonMessages = {
+    [ContactFormStates.UNSENT]: "Send",
+    [ContactFormStates.SENT]: "Sending ...",
+    [ContactFormStates.CONFIRMED]: "Message received",
+  };
 
   function handleSubmit(e: SubmitEvent) {
     e.preventDefault();
     setContactFormState(ContactFormStates.SENT);
-    
+
+    setTimeout(() => setContactFormState(ContactFormStates.CONFIRMED), 2000);
     // send to aws email service...
   }
 
@@ -25,7 +34,12 @@ export default function ContactForm() {
             <h3>Reach out!</h3>
             <input type="text" placeholder="John Doe"></input>
             <input type="email" placeholder="mikehawk@gmail.com"></input>
-            <button type="submit" disabled={contactFormState != ContactFormStates.UNSENT}>send</button>
+            <button
+              type="submit"
+              disabled={contactFormState != ContactFormStates.UNSENT}
+            >
+              {buttonMessages[contactFormState]}
+            </button>
           </div>
           <div className="right">
             <textarea></textarea>
