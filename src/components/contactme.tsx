01e9524512af8e64644dbb355980839d64dfb0f4
Benjamin Curis-Friedman <bencf99@gmail.com>
2026-07-25 01:56:41 -0400

    contact form?

--- src/components/contactme.tsx
+++ src/components/contactme.tsx
@@ -1,19 +1,35 @@
 import "#styles/contactme.css";
+import { useRef, useState } from "react";
+
+enum ContactFormStates {
+  UNSENT,
+  SENT,
+  CONFIRMED,
+}
 
 export default function ContactForm() {
+  const [contactFormState, setContactFormState] = useState(ContactFormStates.UNSENT);
+
+  function handleSubmit(e: SubmitEvent) {
+    e.preventDefault();
+    setContactFormState(ContactFormStates.SENT);
+    
+    // send to aws email service...
+  }
+
   return (
     <>
       <div id="contactForm">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="left">
             <h3>Reach out!</h3>
-            <input type="email"></input>
-            <input></input>
+            <input type="text" placeholder="John Doe"></input>
+            <input type="email" placeholder="mikehawk@gmail.com"></input>
+            <button type="submit" disabled={contactFormState != ContactFormStates.UNSENT}>send</button>
           </div>
           <div className="right">
             <textarea></textarea>
           </div>
-          <button type="submit"></button>
         </form>
       </div>
     </>
