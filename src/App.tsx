aa193126dcffbf75b2109c06c8974c07d43a5043
Benjamin Curis-Friedman <bencf99@gmail.com>
2026-08-05 01:21:38 -0400

    epic underline animations

--- src/App.tsx
+++ src/App.tsx
@@ -16,12 +16,12 @@ function App() {
       <div className="ticks"></div>
 
       <section id="main">
-        <h2>My experience</h2>
+        <h2><span className="hover-line">Experience</span></h2>
         <Experiences />
         <div className="ticks"></div>
-        <h2>My projects</h2>
+        <h2><span className="hover-line">Projects</span></h2>
         <Projects />
-        <h2>Testimonials</h2>
+        <h2><span className="hover-line">Testimonials</span></h2>
       </section>
 
       <section id="contact">
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,34 @@
 import "#styles/App.css";
 import "#styles/navbar.css";
+import { useEffect, useRef } from "react";
 export default function Navbar() {
+  const nameSpanRef = useRef<HTMLSpanElement>(null);
+  useEffect(() => {
+    // This is called an Immediately Invoked Function Expression (IIFE)
+    (async () => {
+      nameSpanRef.current?.classList.add("hovered");
+
+      // This is basically an asyncio.sleep(0.6)
+      await new Promise((resolve) => setTimeout(resolve, 600));
+
+      nameSpanRef.current?.classList.remove("hovered");
+    })();
+  }, []);
   return (
     <div id="navbar">
-      <h1>Benjamin Curis-Friedman</h1>
+      <h1>
+        <span ref={nameSpanRef} className="hover-line hover-line-lr">
+          Benjamin Curis-Friedman
+        </span>
+      </h1>
       <ul className="navbarOptions">
         <li>
           <a href="https://github.com/Benjamincf0" target="_blank">
-            <svg className="button-icon github" role="presentation" aria-hidden="true">
+            <svg
+              className="button-icon github"
+              role="presentation"
+              aria-hidden="true"
+            >
               <use href="/icons.svg#github-icon"></use>
             </svg>
             GitHub
@@ -15,7 +36,11 @@ export default function Navbar() {
         </li>
         <li>
           <a href="https://www.linkedin.com/in/benjaminc-f/" target="_blank">
-            <svg className="button-icon linkedin" role="presentation" aria-hidden="true">
+            <svg
+              className="button-icon linkedin"
+              role="presentation"
+              aria-hidden="true"
+            >
               <use href="/icons.svg#linkedin-icon"></use>
             </svg>
             LinkedIn
@@ -23,7 +48,11 @@ export default function Navbar() {
         </li>
         <li>
           <a href="#contact">
-            <svg className="button-icon social" role="presentation" aria-hidden="true">
+            <svg
+              className="button-icon social"
+              role="presentation"
+              aria-hidden="true"
+            >
               <use href="/icons.svg#social-icon"></use>
             </svg>
             Contact
