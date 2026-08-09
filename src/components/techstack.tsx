d62d3f0060c81410af7b489bc98a7a2254d2f6c6
Benjamin Curis-Friedman <bencf99@gmail.com>
2026-08-08 23:50:43 -0400

    fixed techstack lesgo

--- src/components/techstack.tsx
+++ src/components/techstack.tsx
@@ -1,4 +1,4 @@
-import "#styles/techstack.css"
+import "#styles/techstack.css";
 
 export interface TechStackData {
   name: string;
@@ -6,19 +6,23 @@ export interface TechStackData {
 }
 
 export default function TechStack({ stack }: { stack: TechStackData[] }) {
-  return <ul className="techStack">
-    {stack.map(({ name, img_src }, index) => (
-      <li
-        key={name}
-        style={{
-          left: `${-15 * index + 15 * (stack.length - 1)}px`,
-        }}
-      >
-        <img src={img_src} />
-        <div className="grower">
-          <p>{name}</p>
-        </div>
-      </li>
-    ))}
-  </ul>;
+  return (
+    <ul className="techStack">
+      {stack.map(({ name, img_src }, index) => (
+        <li
+          key={name}
+          style={{
+            left: `${-15 * index + 15 * (stack.length - 1)}px`,
+          }}
+        >
+          <div className="wrapper">
+            <img src={img_src} />
+            <div className="grower">
+              <p>{name}</p>
+            </div>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
 }
