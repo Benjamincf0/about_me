7c371ff0e2f8e352f3b4e9c0cd91dae3a77c869a
Benjamin Curis-Friedman <bencf99@gmail.com>
2026-08-09 23:43:07 -0400

    fixed the techstack with reusable items

--- src/components/techstack.tsx
+++ src/components/techstack.tsx
@@ -1,11 +1,100 @@
 import "#styles/techstack.css";
 
+export const TechItems = {
+  TYPESCRIPT: {
+    name: "Typescript",
+    img_src: "typescript-icon",
+  },
+  GLSL: {
+    name: "glsl",
+    img_src: "glsl-icon",
+  },
+  HTML: {
+    name: "HTML",
+    img_src: "html-icon",
+  },
+  TENSORFLOW: {
+    name: "Tensorflow",
+    img_src: "tensorflow-icon",
+  },
+  REACT: {
+    name: "React",
+    img_src: "react-icon",
+  },
+  PYTHON: {
+    name: "Python",
+    img_src: "python-icon",
+  },
+  NUMPY: {
+    name: "Numpy",
+    img_src: "numpy-icon",
+  },
+  CSS: {
+    name: "CSS",
+    img_src: "css-icon",
+  },
+  C: {
+    name: "C",
+    img_src: "c-icon",
+  },
+  CPP: {
+    name: "C++",
+    img_src: "cpp-icon",
+  },
+  JAVA: {
+    name: "Java",
+    img_src: "java-icon",
+  },
+  JAVASCRIPT: {
+    name: "Javascript",
+    img_src: "javascript-icon",
+  },
+  FIREBASE: {
+    name: "Firebase",
+    img_src: "firebase-icon",
+  },
+  CLAUDE_CODE: {
+    name: "Claude code",
+    img_src: "claude-code-icon",
+  },
+  CODEX: {
+    name: "Codex",
+    img_src: "codex-icon",
+  },
+  PYGAME: {
+    name: "Pygame",
+    img_src: "pygame-icon",
+  },
+  UMPLE: {
+    name: "Umple",
+    img_src: "umple-icon",
+  },
+  JAVASCRIPT: {
+    name: "Javascript",
+    img_src: "javascript-icon",
+  },
+  JAVASCRIPT: {
+    name: "Javascript",
+    img_src: "javascript-icon",
+  },
+  JAVASCRIPT: {
+    name: "Javascript",
+    img_src: "javascript-icon",
+  },
+  JAVASCRIPT: {
+    name: "Javascript",
+    img_src: "javascript-icon",
+  },
+
+  // 'as const' freezes the object so its read-only.
+} as const;
+
 export interface TechStackData {
   name: string;
   img_src: string;
 }
 
-export default function TechStack({ stack }: { stack: TechStackData[] }) {
+export function TechStack({ stack }: { stack: TechStackData[] }) {
   return (
     <ul className="techStack">
       {stack.map(({ name, img_src }, index) => (
@@ -16,7 +105,11 @@ export default function TechStack({ stack }: { stack: TechStackData[] }) {
           }}
         >
           <div className="wrapper">
-            <img src={img_src} />
+            <svg
+              role="presentation"
+            >
+              <use href={"/icons.svg#"+img_src}></use>
+            </svg>
             <div className="grower">
               <p>{name}</p>
             </div>
